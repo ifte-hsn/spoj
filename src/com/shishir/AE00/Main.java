a63f70d187560a2e1a51c678e0674ac548316f95
ifte510 <ifte510@gmail.com>
2020-09-27 13:20:26 +0600

    spoj lastdig and rectengale

---
+++ src/com/shishir/AE00/Main.java
@@ -0,0 +1,57 @@
+package com.spoj.AE00;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	private BufferedReader br;
+	private StringTokenizer st;
+
+	private String next() throws IOException {
+		if (st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine());
+		}
+		return st.nextToken();
+	}
+
+	private int nextInt() throws NumberFormatException, IOException {
+		return Integer.parseInt(next());
+	}
+
+	private void run() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		solve();
+	}
+
+	private void solve() throws IOException {
+		int[] cache = new int[10001];
+
+		for (int i = 0; i <= 3; i++) {
+			cache[i] = i;
+		}
+
+		for (int i = 4; i <= 10000; i++) {
+
+			int j = 1;
+			int count = 0;
+
+			while (j <= Math.sqrt(i)) {
+				if (i % j == 0)
+					count++;
+				j++;
+			}
+
+			cache[i] = cache[i - 1] + count;
+		}
+
+		int n = nextInt();
+
+		System.out.println(cache[n]);
+
+	}
+
+	public static void main(String[] args) throws java.lang.Exception {
+		new Main().run();
+	}
+}
---
+++ src/com/shishir/lastdig/Main.java
@@ -0,0 +1,41 @@
+package com.spoj.lastdig;
+
+import java.util.*;
+public class Main {
+	private static Scanner sc;
+	public static void main(String[] args) {
+		sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0) {
+			int a=sc.nextInt()%10,b=sc.nextInt();int res=0;
+			if(b==0) {System.out.println(1); continue;};
+			
+			switch (a){
+			case 0:
+			case 1:
+			case 6:
+			case 5:
+				res=a;
+				break;
+			case 2:
+				if(b%4==0) res=6;
+				else res=(int) Math.pow(a,b%4)%10;
+				break;
+			case 3:
+			case 7:
+			case 8:
+				res=(int)Math.pow(a,b%4)%10;
+				break;
+			case 4:
+				if(b%2==0) res=6;
+				else res=4;
+				break;
+			case 9:
+				if(b%2==0) res = 1;
+				else res = 9;
+				break;
+			}
+			System.out.println(res);
+		}
+	}
+}
